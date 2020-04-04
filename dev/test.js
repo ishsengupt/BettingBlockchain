1032957453c35cb6a3694bf520d59e524f0fe63b
ishsengupt <shan.engupta@gmail.com>
2020-04-04 15:51:48 -0400

    first commit

---
+++ dev/test.js
@@ -0,0 +1,24 @@
+const Blockchain = require("./blockchain");
+
+const bitcoin = new Blockchain();
+
+const previousBlockHash = "WO249TH894TH3948TH";
+const currenBlockData = [
+  {
+    amount: 10,
+    sender: "OIWOWIE3t4FWEF",
+    recipient: "389Q39034423H0"
+  },
+  {
+    amount: 130,
+    sender: "OIWOWIEsefsFWEF",
+    recipient: "389Q39023H0"
+  },
+  {
+    amount: 102,
+    sender: "OIWOWIEFWwerweEF",
+    recipient: "389Q39023H0"
+  }
+];
+
+console.log(bitcoin);
